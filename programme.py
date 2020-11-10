7fae3f0339ed8350755c24cc3820ca0d14602d05
Florent ESNAULT <esnault.florent.pro@gmail.com>
2020-11-10 17:21:43 +0100

    Ajout d'un moyen pour trouver facilement la position du click.
    Ajout d'un fichier readme avec quelques infos sur ce qu'il faut installer pour lancer le programme.

--- programme.py
+++ programme.py
@@ -23,8 +23,8 @@ class Programme():
     # .................................................................
     # Constantes
 
-    POSITION_BOUTON_REFRESH = (-1393, 73)
-    TEMPS_AVANT_REFRESH = 170
+    POSITION_BOUTON_REFRESH = (-1393, 79)
+    TEMPS_AVANT_REFRESH = 30 # Temps d'attente entre deux clicks, en secondes
 
     # .................................................................
     # Constructeur & méthodes spéciales
@@ -43,22 +43,50 @@ class Programme():
         time.sleep(3)
 
         while True:
-            print("\nAvertissement")
+            print("\nConfigurer la position du bouton refresh du navigateur? (oui/non)")
+            saisie = input()
 
-            print("Lancement du refresh")
-            position_actuelle_souris_x, position_souris_actuelle_y = pyautogui.position()
+            if saisie in ["yes", "y", "oui", "o"]:
+                print("\nClick de test dans 5 secondes...")
+                time.sleep(1.0)
+                print("4 secondes...")
+                time.sleep(1.0)
+                print("3 secondes...")
+                time.sleep(1.0)
+                print("2 secondes...")
+                time.sleep(1.0)
+                print("1 seconde...")
+                time.sleep(1.0)
+                print("Click...")
+                position_souris_x, position_souris_y = pyautogui.position()
+                pyautogui.click(x = self.POSITION_BOUTON_REFRESH[0], y = self.POSITION_BOUTON_REFRESH[1])
+                print(f"Fait. Position de la souris lors du click: ({position_souris_x}, {position_souris_y})")
+
+                print("\nConserver cette position? (oui/non)")
+                saisie = input()
+
+                if saisie in ["yes", "y", "oui", "o"]:
+                    self.POSITION_BOUTON_REFRESH = (position_souris_x, position_souris_y)
+                    print("Nouvelle position enregistrée.")
+
+            elif saisie in ["no", "n", "non", "q", "quitter"]:
+                break
+
+        while True:
+            print("\nLancement du refresh")
+            position_souris_x, position_souris_y = pyautogui.position()
 
             print("Déplacement + click...")
             pyautogui.click(x = self.POSITION_BOUTON_REFRESH[0], y = self.POSITION_BOUTON_REFRESH[1])
             print("Fait.")
 
             print("Retour à la position initiale...")
-            pyautogui.moveTo(x = position_actuelle_souris_x, y = position_souris_actuelle_y)
+            pyautogui.moveTo(x = position_souris_x, y = position_souris_y)
             print("Fait.")
 
             temps_avant_refresh = self.TEMPS_AVANT_REFRESH 
             temps_avant_refresh += random.randint(-10, 10)
-            
+
             time.sleep(temps_avant_refresh)
         
 
---
+++ readme.txt
@@ -0,0 +1,21 @@
+
+Pour pouvoir lancer le script:
+- Installer Python: https://www.python.org/downloads/
+- Configurer Python sous Windows: 
+    - Aller dans Système
+    - Paramètres système avancés
+    - Variables d'environnement...
+    - Dans Variables utilisateur et/ou dans Variables système, ajouter le chemin d'accès de Python et des scripts de base, du style:
+        - C:\Program Files (x86)\Python38-32\
+        - C:\Program Files (x86)\Python38-32\Scripts\
+
+Il est ensuite possible de lancer la commande python depuis n'importe où en console.
+
+Il faut également installer la librairie pyautogui (sert à effectuer les clicks):
+> python -m pip install pyautogui
+
+Pour lancer le script:
+> python programme.py
+
+Normalement c'est tout.
+
